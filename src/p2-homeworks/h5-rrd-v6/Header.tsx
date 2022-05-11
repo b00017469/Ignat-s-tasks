da6e9f7ffdd6a015a57c51b9fb2e212d56534e43
Pavel Morozuk <morozuki@mail.ru>
2022-05-11 15:30:34 +0300

    hw6

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -7,9 +7,9 @@ function Header() {
     return (
         <div className={s.navdiv}>
 
-            <NavLink className={s.link} to={PATH.PRE_JUNIOR}>pre-junior</NavLink>
-            <NavLink className={s.link} to={PATH.JUNIOR}>junior</NavLink>
-            <NavLink className={s.link} to={PATH.JUNIOR_PLUS}>junior+</NavLink>
+            <NavLink className={({ isActive }) => isActive ? `${s.activated}`: `${s.link}`} to={PATH.PRE_JUNIOR}>pre-junior</NavLink>
+            <NavLink className={({ isActive }) => isActive ? `${s.activated}`: `${s.link}`} to={PATH.JUNIOR}>junior</NavLink>
+            <NavLink className={({ isActive }) => isActive ? `${s.activated}`: `${s.link}`} to={PATH.JUNIOR_PLUS}>junior+</NavLink>
             <div className={s.block}></div>
         </div>
     )
