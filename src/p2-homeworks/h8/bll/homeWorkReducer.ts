c1353b66e9c33235c44cb62e6bda42c9d7a01c1d
Pavel Morozuk <morozuki@mail.ru>
2022-06-12 19:22:12 +0300

    hw8

--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,13 +1,27 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+import {UserType} from "../HW8";
+
+type SortActionType = {
+    type: 'sort'
+    payload: string
+}
+type CheckActionType = {
+    type: 'check'
+    payload: number
+}
+
+export const homeWorkReducer = (state: UserType[], action: SortActionType | CheckActionType): UserType[] => {
     switch (action.type) {
         case 'sort': {
-            // need to fix
-            return state
+            if (action.payload === 'up') {
+                return [...state].sort((a, b) => a.name < b.name ? -1 : 1)
+            } else if (action.payload === 'down') {
+                return [...state].sort((a, b) => a.name > b.name ? -1 : 1)
+            } else return state
         }
         case 'check': {
-            // need to fix
-            return state
+            return state.filter(el => el.age > action.payload)
         }
-        default: return state
+        default:
+            return state
     }
 }
